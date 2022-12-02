bd6ee9c35f446043fa9437ded2660352186c040c
Anqinghuahua <57083544+Anqinghuahua@users.noreply.github.com>
2022-12-02 14:14:32 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,11 +21,11 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'o2a6d6RwB4CMZXtOUcForP7ni9Rk',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'zhnqkTNtaSwLe2dPnO4y-75VOoG4RwUZSB9mTzgc_Pc',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '03-08',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'zhnqkTNtaSwLe2dPnO4y-75VOoG4RwUZSB9mTzgc_Pc',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'o2a6d6REPPHoXlqodQm1gqD4FaLY',
     }
   ],
 
